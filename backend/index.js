453da49c89fb114f7279588d916a018325753d61
Nikiforos88 <nikiforosamg@gmail.com>
2024-06-11 07:55:24 +0300

    add swagger

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,11 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./utilities");
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
 app.use(
     cors({
